63277a9d14176c09326dd21999c0e58bcb9afcc9
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-03-13 10:49:12 +0600

    minor ui update

--- medi_queue/lib/util/doctor/doctor_card.dart
+++ medi_queue/lib/util/doctor/doctor_card.dart
@@ -13,72 +13,69 @@ class DoctorCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Doctor doctor = doctorList.where((doctor) => doctor.id == id).first;
-    return Container(
-      margin: EdgeInsets.only(right: 10),
-      child: Center(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-          width: 180,
-          // height: 350,
-          decoration: BoxDecoration(
-            color: glassyColor,
-            borderRadius: BorderRadius.circular(7),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //image
-              ClipOval(
-                child: CachedNetworkImage(
-                  imageUrl: doctor.imageAddress,
-                  placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => Icon(Icons.error),
-                  maxHeightDiskCache:
-                      150, // Set the maximum height for disk-cached images
-                  height: 80,
-                  width: 80,
-                  fit: BoxFit.cover,
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+        width: 180,
+        // height: 350,
+        decoration: BoxDecoration(
+          color: glassyColor,
+          borderRadius: BorderRadius.circular(7),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //image
+            ClipOval(
+              child: CachedNetworkImage(
+                imageUrl: doctor.imageAddress,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                maxHeightDiskCache:
+                    150, // Set the maximum height for disk-cached images
+                height: 80,
+                width: 80,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 7),
+            //starts
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.star,
+                  color: secondaryColor,
+                  size: 18,
                 ),
-              ),
-              SizedBox(height: 7),
-              //starts
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.star,
-                    color: secondaryColor,
-                    size: 18,
+                Padding(
+                  padding: const EdgeInsets.only(top: 2.0),
+                  child: Text(
+                    " ${doctor.rating}",
+                    style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                          fontSize: 13,
+                        ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 2.0),
-                    child: Text(
-                      " ${doctor.rating}",
-                      style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                            fontSize: 14,
-                          ),
-                    ),
+                ),
+              ],
+            ),
+            SizedBox(height: 15),
+            Text(
+              doctor.name,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontSize: 16,
                   ),
-                ],
-              ),
-              SizedBox(height: 15),
-              Text(
-                doctor.name,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                      fontSize: 18,
-                    ),
-              ),
-              SizedBox(height: 4),
-              Text(
-                doctor.desg,
-                style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                      fontSize: 12,
-                    ),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(height: 4),
+            Text(
+              doctor.desg,
+              style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                    fontSize: 12,
+                  ),
+            ),
+          ],
         ),
       ),
     );
